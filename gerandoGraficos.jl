77511466419f84bd9a5eef276fc1c2a03c8c1788
eduardo meneghel <eduardopedromeneghe@outlook.com.br>
2023-03-25 03:21:17 -0300

    feat/simplificando gráfico

--- gerandoGraficos.jl
+++ gerandoGraficos.jl
@@ -1,10 +1,13 @@
 import Pkg
 Pkg.add("Plots")
 using Plots
-
-x = range(0, 10, length=100)
-y = sin.(x)
-y_noisy = @. sin(x) + 0.1*randn()
-
-plot(x, y, label="sin(x)")
-plot!(x, y_noisy, seriestype=:scatter, label="data")
+#posição onde o dado será inserido
+x = [1,2,3,4,5,6,7,8,9,10]
+#valor em linha da posição respectiva que será inserido
+y = [9,8,7,6,5,5,3,2,1,0]
+#valor em ponto da posição respectiva que será inserido
+y_noisy = [1,3,5,7,9,6,8,2,5,9]
+#imprime a linha
+plot(x, y, label="linhas")
+#imprime os pontos
+plot!(x, y_noisy, seriestype=:scatter, label="pontos")
