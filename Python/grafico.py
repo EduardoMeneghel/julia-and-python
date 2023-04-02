1f7c685fcc9b1a758c565536c09a8b00e18596b6
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-04-01 22:09:02 -0300

    feat/gerando gráficos com python

---
+++ Python/grafico.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot
+
+meses = ['JAN', 'FEV', 'MAC', 'ABR', 'MAI', 'JUN']
+valores = [150, 98, 235, 152, 100, 250]
+
+matplotlib.pyplot.plot(meses, valores)
+
+matplotlib.pyplot.show()
